a984882cfa7ccb485b516cc5709e592d5d9631e0
nvnkmrd <56342655+nvnkmrd@users.noreply.github.com>
2019-12-20 19:35:23 +0530

    First commit.

---
+++ web/precache-manifest.8f7676ff37045d7a9af9f7e2fa0c670d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbc45c3581980c889088be3fee8fff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee983a0cc5035de6dd0b",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "9231e9d0bd19023b2cc5",
+    "url": "/static/css/main.3e451c38.chunk.css"
+  },
+  {
+    "revision": "ee983a0cc5035de6dd0b",
+    "url": "/static/js/2.1c2774a2.chunk.js"
+  },
+  {
+    "revision": "9231e9d0bd19023b2cc5",
+    "url": "/static/js/main.1da041bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
