986f3458b41cbac38e59a89f6530590799a80d6e
Naveen Kumar D <naveenkumar_d@photonint.com>
2020-02-10 19:15:26 +0530

    Pricing intellegence deployment.

---
+++ web/precache-manifest.9c437f3d390c9dcbf6175b6757a1e4fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61f0962279509ba263d5824873c6849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba2901a3b1aeae6ac4e6",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "aa8776cd3e2cb6d2e0f4",
+    "url": "/static/css/main.5072cb7f.chunk.css"
+  },
+  {
+    "revision": "ba2901a3b1aeae6ac4e6",
+    "url": "/static/js/2.dc2ebd8d.chunk.js"
+  },
+  {
+    "revision": "aa8776cd3e2cb6d2e0f4",
+    "url": "/static/js/main.a0349c60.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- web/service-worker.js
+++ web/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e21fd824d9dbde43a004d3df464c3de.js"
+  "/precache-manifest.9c437f3d390c9dcbf6175b6757a1e4fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
